7f2f6ad58b4f9a0a0a1be98e986be73e5a54764c
Germain Lefebvre <glefebvre@ineat.fr>
2020-02-02 23:51:09 +0100

    refactor(report): Replace old calcul by new one

--- api/reports.py
+++ api/reports.py
@@ -22,42 +22,6 @@ salary_hour_net = 3.50
 fees_day_net = 3.08
 
 @bp.route("/reports/<int:year>/<int:month>")
-def getBusinessDays(year, month):
-    select_start = str(year)+str(month)+"01"
-    select_end_tmp = dt.date(year, month, 1) + dd.datedelta(months=1)
-    select_end = select_end_tmp.strftime('%Y%m%d')
-
-    db = get_db()
-    data = db.execute("select day from days_off where day >= " + select_start + " and day < " + select_end ).fetchall()
-    db.close()
-
-    days_exception = [dt.date(int(x[0][:4]), int(x[0][4:-2]), int(x[0][-2:])) for x in data]
-
-    start = dt.date( year, month, 1 )
-    end = dt.date(year, month, 1) + dd.datedelta(months=1)
-    holidays_fra = [x[0] for x in holidays.FRA(years=year).items()]
-
-    month_str = dt.date(year, month, 1).strftime("%B")
-    bdays = int(np.busday_count( start, end , weekmask=weekmask_list, holidays=holidays_fra ))
-    bdays_wExceptions = int(np.busday_count( start, end , weekmask=weekmask_list, holidays=holidays_fra+days_exception ))
-    hours_normalByMonth = int(hours_by_day*weekmask_list.count(1)*52/12)
-    fees_month_net = "{0:.2f}".format(fees_day_net*bdays_wExceptions)
-    salary_month_net = str(salary_hour_net*hours_by_day*sum(weekmask_list)*52/12)
-    
-    return {
-        "year": year,
-        "month": month_str,
-        "monthId": month,
-        "businessDays": bdays,
-        "businessDaysWithExceptions": bdays_wExceptions,
-        "normalHours": hours_normalByMonth,
-        "salary": salary_month_net,
-        "fees": fees_month_net
-    }
-
-
-
-@bp.route("/my/reports/<int:year>/<int:month>")
 def getReport(year, month):
     select_start = "{}-{}-{}".format(year, month, "01") # str(year)+str(month)+"01"
     select_end_tmp = dt.date(year, month, 1) + dd.datedelta(months=1)
