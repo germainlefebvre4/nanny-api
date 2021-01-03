60aee7ca59764baa313459ee10d68ab80eaff762
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2021-01-03 01:47:19 +0100

    feat(email): Change email content

--- app/core/config.py
+++ app/core/config.py
@@ -23,7 +23,7 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    PROJECT_NAME: str = "nanny-api"
+    PROJECT_NAME: str = "Nanny"
     SENTRY_DSN: Optional[HttpUrl] = ""
 
     @validator("SENTRY_DSN", pre=True)
